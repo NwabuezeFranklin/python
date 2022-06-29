1afaf803ecb8d5284f67f01158f2f3915f15ed5b
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-06-29 23:10:48 +0100

    What day was it

---
+++ controlstatements.py
@@ -0,0 +1,18 @@
+today = "wednesday"
+
+if today == 'monday':
+    print('It is workday')
+elif today == 'tuesday':
+    print('it is workday')
+elif today == 'wednesday':
+    print('it\'s wednesday')
+elif today== 'thursday':
+    print('take a short-break')
+elif today == 'friday':
+    print('tgif')
+elif today == 'saturday':
+    print('Rest easy')
+elif today == 'sunday':
+    print('we start again tomorrow')
+else:
+    print("what is wrong. lol")
