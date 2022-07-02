40dfc262cd84ec7640d183d67da7f1fb1730707c
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-02 22:30:25 +0100

    Functions next stop banas21

---
+++ banas14.py
@@ -0,0 +1,16 @@
+#  functions
+def change_me(name):
+    name = 'Chioma'
+    return name
+
+name = ''
+name = 'Jacinta'
+print('Seems to be changed ',change_me(name))
+
+name1 = "Tammy"
+change_me(name1)
+print('Changed or not here i come',change_me(name1))
+
+name2 = "Tammy"
+trick = change_me(name2)
+print(trick)
