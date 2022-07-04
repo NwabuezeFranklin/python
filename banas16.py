699465240c704ee80224a96721e9c6229d5e0fdf
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-04 21:35:08 +0100

    The E

---
+++ banas16.py
@@ -0,0 +1,15 @@
+for i in range(11):
+    print(i,end="")
+print()
+for i in range(3):
+    print(i)
+for i in range(11):
+    print(i,end="")
+print()
+for i in range(3):
+    print(i)
+for i in range(11):
+    print(i,end="")
+
+
+
