c3884d82b3a73a51d7b8bc21e17f3bc45f99c8a0
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-04 21:35:51 +0100

    The old tricks of .is

---
+++ banas17.py
@@ -0,0 +1,7 @@
+letter = "T"
+num = '3'
+print('Is t an alphabet', letter.isalpha())
+print('Is T  a num or an alphabet', letter.isalnum())
+print('Is num value a number',num.isdigit())
+print('Is t a lowercase: ',letter.islower())
+print('is T an uppercase',letter.isupper())
