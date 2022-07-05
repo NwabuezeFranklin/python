448c47e57552d3db5592d583bd800268ebd93e4c
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-05 21:18:24 +0100

    The String manipulations

---
+++ banasManipulations.py
@@ -0,0 +1,8 @@
+string = "This was Important"
+print(len(string))
+print(string[9:] +" " + string[0:9])
+print('Hello ' * 3)
+for char in string:
+    print(char)
+for m in string:
+    print(int(ord(m)),)
