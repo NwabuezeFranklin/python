e2c90840579fab69cd07074d1e18d9efc56db8fc
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-17 20:11:31 +0100

    If name == main

---
+++ banas61.py
@@ -0,0 +1,10 @@
+import threading
+
+def firstthread():
+    print("Succesful")
+
+if __name__== "__main__":
+    print("Executing the main first")
+    firstthread()
+
+#  There was no need to import threading
