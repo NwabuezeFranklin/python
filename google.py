86555069dc9e73c1900294752089a0ac4de3f022
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-20 21:10:55 +0100

    Enter into coursera

---
+++ google.py
@@ -0,0 +1,17 @@
+def calculate_storage(filesize):
+    block_size = 4096
+    # Use floor division to calculate how many blocks are fully occupied
+    full_blocks = filesize//block_size
+    # Use the modulo operator to check whether there's any remainder
+    partial_block_remainder =block_size%filesize
+    # Depending on whether there's a remainder or not, return
+    # the total number of bytes required to allocate enough blocks
+    # to store your data.
+    if partial_block_remainder > 0:
+        return block_size * 2
+    return block_size
+
+print(calculate_storage(1))    # Should be 4096
+print(calculate_storage(4096)) # Should be 4096
+print(calculate_storage(4097)) # Should be 8192
+print(calculate_storage(6000)) # Should be 8192
