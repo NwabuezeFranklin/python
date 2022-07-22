a6e636f92b1a3e3f0927a2e5d3344d58c70f6693
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-22 23:19:25 +0100

    Longest word in a string

---
+++ google4.py
@@ -0,0 +1,12 @@
+def longest_word(word1, word2, word3):
+	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word2) >= len(word1) and len(word2) >= len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
