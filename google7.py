d33be85910e3948adcb7c708bd5308e7765b95e4
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-22 23:22:24 +0100

    Sets the premier league home and away matches

---
+++ google7.py
@@ -0,0 +1,5 @@
+teams = [ 'Dragons', 'Wolves', 'Pandas', 'Unicorns']
+for home_team in teams:
+    for away_team in teams:
+        if home_team != away_team:
+            print(home_team ," vs ", away_team)
