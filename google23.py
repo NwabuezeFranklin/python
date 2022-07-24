8c3a6814ca0876fd3618f7f2aec8477db399ec63
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-24 23:12:09 +0100

    Strange

---
+++ google23.py
@@ -0,0 +1,7 @@
+def group_list(group, users):
+  members = ", ".join([str(m) for m in users])
+  return group +": "+ members
+
+print(group_list("Marketing", ["Mike", "Karen", "Jake", "Tasha"])) # Should be "Marketing: Mike, Karen, Jake, Tasha"
+print(group_list("Engineering", ["Kim", "Jay", "Tom"])) # Should be "Engineering: Kim, Jay, Tom"
+print(group_list("Users", "")) # Should be "Users:"
