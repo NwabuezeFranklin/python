89e797cd7de03ea5ce04e5b758aee0c14446d5f2
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-31 02:41:02 +0100

    Create a File and iterate through the lines

---
+++ google38.py
@@ -0,0 +1,10 @@
+with open ("new.txt") as file:
+    for line in file:
+        print(line)
+
+
+with open ("new.txt") as file:
+    for line in file:
+        print(line.strip().upper())
+
+file open()
