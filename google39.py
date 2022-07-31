3d457d87547d2e14ce53b0150e9e4110d329165a
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-07-31 02:43:32 +0100

    File handling Mastered

---
+++ google39.py
@@ -0,0 +1,34 @@
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
+new_guests = ["Sam", "Danielle", "Jacob"]
+
+with open("guests.txt", "a") as guests:
+    for i in new_guests:
+        guests.write(i + "\n")
+
+guests.close()
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
+
+checked_out=["Andrea", "Manuel", "Khalid"]
+temp_list=[]
+
+with open("guests.txt", "r") as guests:
+    for g in guests:
+        temp_list.append(g.strip())
+
+with open("guests.txt", "w") as guests:
+    for name in temp_list:
+        if name not in checked_out:
+            guests.write(name + "\n")
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
+
+with open("guests.txt") as guests:
+    for line in guests:
+        print(line)
