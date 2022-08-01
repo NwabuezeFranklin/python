12ada3b725b8dcaba75fc7d4b2bbdf85fe7d5e5d
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-08-01 21:36:22 +0100

    Regex re.search

---
+++ google53.py
@@ -0,0 +1,12 @@
+import re
+def extract_pid(log_line):
+    regex = r"\[(\d{0,})\]"
+    result = re.search(regex, log_line)
+    if result is None:
+        return None
+    return "{} ".format(result[1])
+
+print(extract_pid("July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade")) # 12345
+print(extract_pid("99 elephants in a [cage]")) # None
+print(extract_pid("A string that also has numbers [34567] but no uppercase message")) # 34567
+print(extract_pid("July 31 08:08:08 mycomputer new_process[67890]: RUNNING Performing backup")) # 67890
