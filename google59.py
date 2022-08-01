4eabd54dcec4bb31ec1a3d23329c553e60b8244b
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-08-01 22:06:43 +0100

    Python to C

---
+++ google59.py
@@ -0,0 +1,13 @@
+import re
+def transform_comments(line_of_code):
+  result = re.sub(r"(#)+",r"//",line_of_code)
+  return result
+
+print(transform_comments("### Start of program"))
+# Should be "// Start of program"
+print(transform_comments("  number = 0   ## Initialize the variable"))
+# Should be "  number = 0   // Initialize the variable"
+print(transform_comments("  number += 1   # Increment the variable"))
+# Should be "  number += 1   // Increment the variable"
+print(transform_comments("  return(number)"))
+# Should be "  return(number)"
