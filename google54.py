0a82e4e3c560155d65059f60256146645295b4ab
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-08-01 21:40:08 +0100

    Regex find number and error message

---
+++ google54.py
@@ -0,0 +1,12 @@
+import re
+def extract_pid(log_line):
+    regex = r"\[(\d+)\] , (^A-Z.*A_Z$)"
+    result = re.search(regex, log_line)
+    if result is None:
+        return None
+    return "{} ({})".format(result[1],result[2])
+
+print(extract_pid("July 31 07:51:48 mycomputer bad_process[12345]: ERROR Performing package upgrade")) # 12345 (ERROR)
+print(extract_pid("99 elephants in a [cage]")) # None
+print(extract_pid("A string that also has numbers [34567] but no uppercase message")) # None
+print(extract_pid("July 31 08:08:08 mycomputer new_process[67890]: RUNNING Performing backup")) # 67890 (RUNNING)
