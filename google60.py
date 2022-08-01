2a41672f3bc74f3b83daa1ab83ae8f701b6cad1a
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-08-01 22:08:29 +0100

    Convert Phone numbers

---
+++ google60.py
@@ -0,0 +1,9 @@
+import re
+def convert_phone_number(phone):
+  result = re.sub(r"(?<!\S)(\d{3})-",r"(\1)",phone)
+  return result
+
+print(convert_phone_number("My number is 212-345-9999.")) # My number is (212) 345-9999.
+print(convert_phone_number("Please call 888-555-1234")) # Please call (888) 555-1234
+print(convert_phone_number("123-123-12345")) # 123-123-12345
+print(convert_phone_number("Phone number of Buckingham Palace is +44 303 123 7300")) # Phone number of Buckingham Palace is +44 303 123 7300
