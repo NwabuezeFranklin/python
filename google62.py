bbdce584745bd1e3996f917a40fe33ff678049b5
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-08-06 21:37:56 +0100

    Sub-processes Introduction

---
+++ google62.py
@@ -0,0 +1,8 @@
+"""
+A system command that sends ICMP packets can be executed within a script by using which of the following?
+subprocess.run
+or we can easily run the "ping" command
+"""
+import subprocess
+print(subprocess.run(["date"])) #to be updated
+
