214a06a747b382213f06aaf3a5cbc3731442dee3
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-08-12 04:04:41 +0100

    Regex update

---
+++ google63.py
@@ -0,0 +1,9 @@
+import re
+def show_time_of_pid(line):
+  pattern =
+  result = re.search(pattern, line)
+  return result
+
+print(show_time_of_pid("Jul 6 14:01:23 computer.name CRON[29440]: USER (good_user)")) # Jul 6 14:01:23 pid:29440
+
+print(show_time_of_pid("Jul 6 14:02:08 computer.name jam_tag=psim[29187]: (UUID:006)")) # Jul 6 14:02:08 pid:29187
