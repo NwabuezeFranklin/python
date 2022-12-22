30fba59619201cbae0cb7f21f703af2529297195
Nwabueze Franklin <99493414+NwabuezeFranklin@users.noreply.github.com>
2022-12-22 21:04:36 +0100

    Update banas 4.py

--- banas 4.py
+++ banas 4.py
@@ -1,4 +1,6 @@
-# round to two decimals only
+# round to two decimals only...
 f = input('Enter a floating number ')
 f = float(f)
 print('Your float to 2 decimal places = {:.2f}'.format(f))
+print("Straight back to it")
+# This was updated
